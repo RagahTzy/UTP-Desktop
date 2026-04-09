be6afac954b8a24e7353faaf0ef7ff51b4ce39e9
mourenemillie <lifeasnanaw@gmail.com>
2026-04-09 13:56:21 +0700

    Menambahkan UI Team frame

--- UTP Desktop/Team.vb
+++ UTP Desktop/Team.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Team
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs)
 
+    End Sub
+
+    Private Sub DataGridViewTeam_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridViewTeam.CellContentClick
+
+    End Sub
 End Class

de1526edec0b4630abaa3f2fb987783d249ed3db
Haruesaki <ahmaddonijalaludin@gmail.com>
2026-04-30 12:58:47 +0700

    ngebenerin tampilan kumite

--- UTP Desktop/Kumite.vb
+++ UTP Desktop/Kumite.vb
@@ -3,11 +3,31 @@
 
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles ResetTimer.Click
 
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
+
+    Private Sub LabelScoreAO_Click(sender As Object, e As EventArgs) Handles LabelScoreAO.Click
+
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles ResetTimer.Click
+
+    End Sub
+
+    Private Sub PnlAkaInfo_Paint(sender As Object, e As PaintEventArgs) Handles PnlAkaInfo.Paint
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label1_Click_1(sender As Object, e As EventArgs) Handles Label1.Click
 
     End Sub
 End Class
